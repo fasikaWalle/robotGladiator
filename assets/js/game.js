c109ae5737777ef33dc246968c77cdc897b3826d
fasikaWalle <fasikabini12@gmail.com>
2020-12-14 09:18:20 -0600

    user input accepted

---
+++ assets/js/game.js
@@ -0,0 +1,11 @@
+fight();
+window.alert('This is an alert! JavaScript is running!');
+function fight() {
+  window.alert('The fight has begun!');
+}
+var playerName = window.prompt("What is your robot's name?");
+//window.alert(playerName);
+console.log(playerName);
+console.log('This logs a string, good for leaving yourself a message');
+console.log(10 + 10);
+console.log("our robot's name is " + ' ' + playerName);
