987a1ab022cb6ba16a025960b5b4878a101f9925
fasikaWalle <fasikabini12@gmail.com>
2020-12-15 14:01:15 -0600

    add comments

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,9 @@
 var EnemyName = "roborto";
 var playerName = window.prompt("insert your name");
-var playerHealth = 100;
+var playerHealth = 200;
 var playerAttack = 10;
 var enemyAttack = [10,20,30];
-var enemyMoney = [10,34,45];
+var playerMoney = 20;
 var enemyHealth=[30,20,30];
 var enemyName = ["roborto", "alan", "k'naan"];
 var fight = function () {
@@ -14,17 +14,24 @@ var fight = function () {
             while (enemyHealth[i] != 0 && playerHealth != 0) {
                 enemyHealth[i] = enemyHealth[i] - playerAttack;
                 if (enemyHealth[i] != 0) {
-                    alert(playerName + " " + "attack" + " " + enemyName[i] + " " + "the remaining health of"+ enemyHealth[i]+" "+"is:" + " " + enemyHealth[i]);
+                    //player defeat the enemy
+                    alert(playerName + " " + "attack" + " " + enemyName[i] + " " + "the remaining health of"+ " " +enemyHealth[i]+" "+"is:" + " " + enemyHealth[i]);
+                   console.log(playerName + " " + "attack" + " " + enemyName[i] + " " + "the remaining health of"+" "+ enemyHealth[i]+" "+"is:" + " " + enemyHealth[i]);
                 } else {
+                    //enemy loose
                     alert(enemyName[i] + " " + "has died!");
+                    console.log(enemyName[i] + " " + "has died!");
                     break;
                 }
-                console.log(enemyName[i]);
                 playerHealth = playerHealth - enemyAttack[i];
                 if (playerHealth != 0) {
-                    alert(enemyName[i] + " " + "attack" + " " + playerName + " " + "the remaining health of player is:" + " " + playerHealth);
+                    //enemy attack player 
+                    alert(enemyName[i] + " " + "attack" + " " + playerName + " " + "the remaining health of "+" "+playerName+" "+" is:" + " " + playerHealth);
+                   console.log(enemyName[i] + " " + "attack" + " " + playerName + " " + "the remaining health of player is:" + " " + playerHealth);
                 } else {
+                    //player loose
                     alert(playerName + " " + "has died!");
+                    console.log(playerName + " " + "has died!");
                     break;
 
                 }
@@ -32,8 +39,10 @@ var fight = function () {
             }
         } else if (start == "skip" || start == "SKIP") {
             if (window.confirm("are yo sure to leave the fight")) {
-                enemyMoney[i] = enemyMoney[i] - 10;
-                alert("the remaining money of" + "  " + EnemyName[i] + "  " + "is:" + "  " + enemyMoney[i]);
+                playerMoney = playerMoney - 10;
+                alert("the remaining money of" + "  " + playerName + "  " + "is:" + "  " +  playerMoney);
+                console.log("the remaining money of" + "  " + playerName + "  " + "is:" + "  " +  playerMoney);
+                break;
             } else {
                 fight();
             }
@@ -43,6 +52,4 @@ var fight = function () {
         }
     }
 }
-
-
-console.log(fight());
+fight();
